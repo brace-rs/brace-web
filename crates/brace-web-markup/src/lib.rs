76795458cdc6f5a7bc4afb3989729f443894940a
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-09-30 16:00:04 +0100

    Add base implementation of text and element nodes

--- crates/brace-web-markup/src/lib.rs
+++ crates/brace-web-markup/src/lib.rs
@@ -1,1 +1,54 @@
+pub enum Node {
+    Text(Text),
+    Element(Element),
+}
 
+pub struct Text {
+    pub value: String,
+}
+
+impl Text {
+    pub fn new<T>(text: T) -> Self
+    where
+        T: Into<String>,
+    {
+        Self { value: text.into() }
+    }
+}
+
+pub struct Element {
+    pub tag: String,
+    pub nodes: Vec<Node>,
+}
+
+impl Element {
+    pub fn new<T>(tag: T) -> Self
+    where
+        T: Into<String>,
+    {
+        Self {
+            tag: tag.into(),
+            nodes: Vec::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Element, Node, Text};
+
+    #[test]
+    fn test_node_tree() {
+        let mut html = Element::new("html");
+        let mut body = Element::new("body");
+        let text = Text::new("Hello world");
+
+        body.nodes.push(Node::Text(text));
+
+        assert_eq!(body.nodes.len(), 1);
+
+        html.nodes.push(Node::Element(body));
+
+        assert_eq!(html.nodes.len(), 1);
+    }
+}
